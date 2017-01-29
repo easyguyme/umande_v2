5bf9e70e5adb42bcef3e110cc15b1a3abd913ec8
Imran <wangara88@gmail.com>
2017-01-29 03:27:13 +0300

    Segregated wardwise

---
+++ admin/makina/extra.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../../plugins/daterangepicker/daterangepicker.css">
+<!-- bootstrap datepicker -->
+<link rel="stylesheet" href="../../plugins/datepicker/datepicker3.css">
+<!-- iCheck for checkboxes and radio inputs -->
+<link rel="stylesheet" href="../../plugins/iCheck/all.css">
+<!-- Bootstrap Color Picker -->
+<link rel="stylesheet" href="../../plugins/colorpicker/bootstrap-colorpicker.min.css">
+<!-- Bootstrap time Picker -->
+<link rel="stylesheet" href="../../plugins/timepicker/bootstrap-timepicker.min.css">
+<!-- Select2 -->
+<link rel="stylesheet" href="../../plugins/select2/select2.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
---
+++ admin/makina/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Developed by <a href="#">Mitchsofts</a>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Umande Trust</a>.</strong> All rights reserved.
+</footer>
